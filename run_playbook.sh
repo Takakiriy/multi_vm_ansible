e3173ceb3495087d4481ee66a784977523f169be
Takakiri <takakiriy@gmail.com>
2021-08-29 17:15:15 +0900

    ANSIBLE_FORCE_COLOR

--- run_playbook.sh
+++ run_playbook.sh
@@ -3,5 +3,5 @@ control_local_port="2259"
 
 ssh  vagrant@localhost  -p $control_local_port  -i $control_private_key \
     "cd /vagrant  &&" \
-    'ANSIBLE_INVENTORY="/tmp/vagrant-ansible/inventory/vagrant_ansible_local_inventory" ' \
+    'ANSIBLE_FORCE_COLOR=true  ANSIBLE_INVENTORY="/tmp/vagrant-ansible/inventory/vagrant_ansible_local_inventory" ' \
     "ansible-playbook  playbook.yml --diff -v"
